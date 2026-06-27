029be3a0df98a719857b73a4253ceb54621f2d75
Shreyansh <handeshreyansh@gmail.com>
2026-06-28 00:12:39 +0530

    Add files via upload

--- adv traffic management.py
+++ adv traffic management.py
@@ -11,17 +11,34 @@ roads={
       "road3":road3,
        "road4":road4
     }
-sorted_roads= sorted(roads.items(),
+while True:
+            sorted_roads= sorted(roads.items(),
                      key=lambda x:x[1],
                      reverse=True)
-for road,vehicles in sorted_roads:
+            for road,vehicles in sorted_roads:
 
-    if vehicles>=50:
-        green_time=10
-    elif vehicles>=30:
-        green_time=7
-    else:
-        green_time = 5
+                 if vehicles>=50:
+                    green_time=10
+                 elif vehicles>=30:
+                      green_time=7
+                 else:
+                     green_time = 5
     
-    print("GREEN",road)
-
+                 print("GREEN",road)
+                 for i in range(green_time,0,-1): 
+                     print("TIME LEFT",i)
+                     time.sleep(1)
+                 print("YELLOW",road)
+                 time.sleep(2)
+                 print("RED",road)
+                 crossed=20
+                 vehicles=vehicles-crossed
+                 if vehicles <0:
+                      vehicles = 0
+                 roads[road]=vehicles
+                 print(road,"remaining vehicles",vehicles)
+                 total=sum(roads.values())
+                 if total==0:
+                    print("\n allRoads are clear now")
+                    print("Traffic Normal")
+                    break
